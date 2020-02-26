34f893ec9b4d009300d5a32bcda7bfb9cfbb713a
AsianJohnBoi <Jbssantias@hotmail.com>
2020-02-26 22:19:54 +1100

    more problems solved

---
+++ Algorithms/CompareTriplets.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    var a1 = 0;
+    var b1 = 0;
+
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            a1++;
+        } else if (a[i] < b[i]) {
+            b1++;
+        }
+    }
+    var result = [a1, b1];
+    return result;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
