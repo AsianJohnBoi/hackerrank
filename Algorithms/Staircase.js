15e21ebe9f5525136c97252f65fc97b6da1549d7
AsianJohnBoi <Jbssantias@hotmail.com>
2020-02-26 22:21:15 +1100

    staircase

---
+++ Algorithms/Staircase.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function staircase(n) {
+    var space = " ";
+    var hash = "#";
+    var rep = 1;
+    for (var j = n - 1; j >= 0; j--) {
+        console.log(space.repeat(j) + hash.repeat(rep));
+        rep++;
+    }
+
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
