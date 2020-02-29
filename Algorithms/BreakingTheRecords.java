09bffdeacf724830d57069ced0ba8a4e71a4b88c
AsianJohnBoi <Jbssantias@hotmail.com>
2020-02-29 14:51:40 +1100

    breaking the record algorithm

---
+++ Algorithms/BreakingTheRecords.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int highBreak = 0;
+        int lowBreak = 0;
+        int highScore = scores[0];
+        int lowScore = scores[0];
+
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] > highScore) {
+                highBreak++;
+                highScore = scores[i];
+            } else if (scores[i] < lowScore) {
+                lowBreak++;
+                lowScore = scores[i];
+            }
+        }
+
+        int record[] = {highBreak, lowBreak};
+        return record;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
