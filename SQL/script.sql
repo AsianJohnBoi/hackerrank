96a7d8da8bafded6a2dc6ce810b613dd1c270aea
AsianJohnBoi <Jbssantias@hotmail.com>
2020-03-04 21:30:42 +1100

    weather obs station 4, 6, 7

--- SQL/script.sql
+++ SQL/script.sql
@@ -4,12 +4,12 @@ SELECT * FROM city;
 -- Japanese Cities' Names
 SELECT name 
 FROM city 
-where countrycode='JPN';
+WHERE countrycode='JPN';
 
 -- Japanese Cities' Attributes
 SELECT * 
 FROM city 
-where countrycode='JPN';
+WHERE countrycode='JPN';
 
 -- Revising the Select Query I
 SELECT * 
@@ -19,7 +19,7 @@ WHERE countrycode='USA' AND population > 100000;
 -- Revising the Select Query II
 SELECT name 
 FROM city 
-where countrycode='USA' AND population > 120000;
+WHERE countrycode='USA' AND population > 120000;
 
 -- Weather Observation Station 1
 SELECT city, state 
@@ -30,5 +30,15 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE MOD(ID, 2) = 0;
 
--- Weather Observation Station 3
-SELECT COUNT(*) - COUNT(DISTINCT city) FROM station;
+-- Weather Observation Station 4
+SELECT COUNT(*) - COUNT(DISTINCT city) 
+FROM station;
+
+-- Weather Observation Station 6
+SELECT unique city FROM station 
+WHERE city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%'OR city LIKE 'U%';
+
+-- Weather Observation Station 7
+SELECT unique city FROM station 
+WHERE city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o'OR city LIKE '%u';
+
