b5f9959bb9e42b977d547e812c20013909ea6d8d
AsianJohnBoi <Jbssantias@hotmail.com>
2020-05-29 14:01:31 +1000

    enhanced solution

--- Algorithms/CompareTriplets.js
+++ Algorithms/CompareTriplets.js
@@ -24,19 +24,15 @@ function readLine() {
 
 // Complete the compareTriplets function below.
 function compareTriplets(a, b) {
-    var a1 = 0;
-    var b1 = 0;
-
+    var res = [0, 0];
     for (var i = 0; i < a.length; i++) {
         if (a[i] > b[i]) {
-            a1++;
+            res[0] += 1;
         } else if (a[i] < b[i]) {
-            b1++;
+            res[1] += 1;
         }
     }
-    var result = [a1, b1];
-    return result;
-
+    return res;
 
 }
 
